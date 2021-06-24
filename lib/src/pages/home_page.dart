abed346137c85e67af76b2f6580215f5f3f8e7de
Kestler Barrios <luiseduardo3213@gmail.com>
2021-06-24 14:16:03 -0600

    Integrando Swiper

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,11 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Hola Mundoooo!!!!!!!!!!!!!!!!1'),
+      appBar: AppBar(
+        title: Text('Peliculas  App'),
+        backgroundColor: Colors.indigoAccent,
+        actions: [IconButton(icon: Icon(Icons.search), onPressed: () {})],
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            _swiperTarjetas(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _swiperTarjetas() {
+    return Container(
+      padding: EdgeInsets.only(top: 10),
+      width: double.infinity,
+      height: 300,
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network(
+            'http://via.placeholder.com/350x150',
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: 3,
+        itemWidth: 200.0,
+        layout: SwiperLayout.STACK,
+        // pagination: SwiperPagination(),
+        // control: SwiperControl(),
+      ),
     );
   }
 }
