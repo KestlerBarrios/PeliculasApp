1e60aecd33123cd9c4262cfbc0db475bb78cbf2c
Kestler Barrios <luiseduardo3213@gmail.com>
2021-06-24 13:21:52 -0600

    Inicio de la App

---
+++ lib/src/routes/routes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/src/pages/home_page.dart';
+
+Map<String, WidgetBuilder> getRoutes() {
+  return {
+  '/': (BuildContext context) =>HomePage(),
+  };
+}
