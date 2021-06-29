bcb1361b4729d59837486289b787acc3b4e57e12
Kestler Barrios <luiseduardo3213@gmail.com>
2021-06-29 17:08:33 -0600

    BuildSuggestions y BuildResults

--- lib/src/search/search_delegate.dart
+++ lib/src/search/search_delegate.dart
@@ -1,29 +1,84 @@
 import 'package:flutter/material.dart';
 
+class DataSearch extends SearchDelegate {
+  String seleccion = '';
 
-class DataSearch extends SearchDelegate{
+  final peliculas = [
+    'Batman',
+    'Capitana Marve',
+    'Shazam',
+    'IronMan',
+    'IronMan 2',
+    'IronMan 3',
+    'IronMan 4',
+    'IronMan 5',
+  ];
+  final peliculasRecientes = [
+    'SpiderMan',
+    'Capitan America',
+  ];
+
   @override
   List<Widget> buildActions(BuildContext context) {
-      // Acciones del AppBar
-      throw UnimplementedError();
-    }
-  
-    @override
-    Widget buildLeading(BuildContext context) {
-      // Icono a la izquierda del AppBar
-      throw UnimplementedError();
-    }
-  
-    @override
-    Widget buildResults(BuildContext context) {
-      // Crea los resultados a mostrar
-      throw UnimplementedError();
-    }
-  
-    @override
-    Widget buildSuggestions(BuildContext context) {
-    // Sugerencias cuando la persona escribe
-    throw UnimplementedError();
+    // Acciones del AppBar
+    return [
+      IconButton(
+          icon: Icon(Icons.clear),
+          onPressed: () {
+            query = '';
+          }),
+    ];
   }
 
+  @override
+  Widget buildLeading(BuildContext context) {
+    // Icono a la izquierda del AppBar
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+      onPressed: () => close(context, null),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // Crea los resultados a mostrar
+    return Center(
+      child: Container(
+        height: 100.0,
+        width: 100.0,
+        color: Colors.amber,
+        child: Text(seleccion),
+      ),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // Sugerencias cuando la persona escribe
+
+    final listaSugerida = (query.isEmpty)
+        ? peliculasRecientes
+        : peliculas
+            .where(
+              (p) => p.toLowerCase().startsWith(query.toLowerCase()),
+            )
+            .toList();
+
+    return ListView.builder(
+      itemCount: peliculasRecientes.length,
+      itemBuilder: (context, i) {
+        return ListTile(
+          leading: Icon(Icons.movie),
+          title: Text(listaSugerida[i]),
+          onTap: () {
+            seleccion = listaSugerida[i];
+            showResults(context);
+          },
+        );
+      },
+    );
+  }
 }
